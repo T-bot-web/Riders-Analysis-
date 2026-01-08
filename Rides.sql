2163c4e54c9eaa5fd377d06142f6bd0e9cf82e00
T-bot-web <shagungoswami1@gmail.com>
2026-01-08 16:19:04 +0530

    Add files via upload

---
+++ Rides.sql
@@ -0,0 +1,25 @@
+-- CREATE DATABASE bikeriders_db; 
+USE bikeriders_db;
+-- CREATE TABLE bikeriders (
+-- Rider_id INT PRIMARY KEY,
+-- Date DATE,
+-- Location VARCHAR(100),
+-- Rides INT,
+-- On_Time_Rides INT,
+-- Cancelled_rides INT,
+-- Missed_rides INT,
+-- Distance_km INT,
+-- Overhead_distance_km INT,
+-- Earnings INT,
+-- Peak_bonus INT,
+-- Waiting_earnings INT,
+-- Fuel_cost INT,
+-- Idle_minutes REAL,
+-- Work_hours REAL,
+-- Customer_rating FLOAT,
+-- Avg_speed_kmph INT,
+-- Network_downtime INT
+-- );
+SELECT *FROM bikeriders;
+-- ALTER TABLE bikeriders MODIFY COLUMN Rider_id VARCHAR(100);
+
